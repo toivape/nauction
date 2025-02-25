041bbca74c2deded9ed7edaa87abba037acccbf5
Zornak the Ruthless <petri.toivari@nitor.com>
2025-02-25 16:34:00 +0200

    Commit

--- src/test/kotlin/com/nitor/nauction/PlaywrightTest.kt
+++ src/test/kotlin/com/nitor/nauction/PlaywrightTest.kt
@@ -1,9 +1,14 @@
 package com.nitor.nauction
 
+import com.microsoft.playwright.Browser
 import com.microsoft.playwright.Page
+import com.microsoft.playwright.Page.WaitForSelectorOptions
 import com.microsoft.playwright.Playwright
+import com.microsoft.playwright.options.LoadState
 import com.microsoft.playwright.options.WaitForSelectorState
+import io.kotest.assertions.arrow.core.shouldBeRight
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldContain
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
@@ -12,10 +17,17 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
 import org.springframework.boot.test.web.server.LocalServerPort
+import org.springframework.test.context.ActiveProfiles
+import java.math.BigDecimal
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ActiveProfiles("test")
 class PlaywrightTest(@Autowired val bidService: BidService) {
+
+    companion object {
+        private const val AUCTION_ITEM_ID = "b030b21b-73f9-40ff-8518-4a45f2c9b769"
+    }
 
     @LocalServerPort
     private var port: Int = 0
@@ -35,34 +47,47 @@ class PlaywrightTest(@Autowired val bidService: BidService) {
         playwright?.close()
     }
 
+    fun Browser.frontpage(): Page {
+        val context = this.newContext()
+        context.setDefaultTimeout(1000.0) // Set default timeout to 1 second
+        val page = context.newPage()
+        page.navigate("$baseUrl/")
+        return page
+    }
+
+    fun Browser.openAuctionItemModal(): Page {
+        val page = frontpage()
+
+        // Click Bid button to open auction item in modal
+        page.locator("[data-bs-itemid='$AUCTION_ITEM_ID']").click()
+        page.waitForSelector(".modal.show")
+        return page
+    }
+
+    fun Page.placeBid(amount: String) {
+        locator("#bid-amount").fill(amount)
+        locator("#submit-bid").click()
+    }
+
     @Test
     fun `Playwright can open front page`() {
         playwright!!.chromium().launch().use { browser ->
-            val page = browser.newPage()
-            page.navigate("$baseUrl/")
+            val page = browser.frontpage()
             //page.screenshot(Page.ScreenshotOptions().setPath(Paths.get("example.png")))
             page.title() shouldBe "Nitor Auction"
         }
     }
 
     @Test
-    fun `Open modal window to make a bid`() {
-        val auctionItemId = "b030b21b-73f9-40ff-8518-4a45f2c9b769"
+    fun `Open auction item window with latest bid data`() {
         playwright!!.chromium().launch().use { browser ->
-            val page = browser.newPage()
-            page.navigate("$baseUrl/")
-
-            val button = page.locator("[data-bs-itemid='$auctionItemId']")
-            button.click()
-
-            // Wait for the modal to be visible
-            page.waitForSelector(".modal.show")
+            val page = browser.openAuctionItemModal()
 
             // Verify that auctionItemId and latestBidId are set correctly
             val auctionItemIdTag = page.locator("#item-id")
-            auctionItemIdTag.getAttribute("value").shouldBe(auctionItemId)
+            auctionItemIdTag.getAttribute("value").shouldBe(AUCTION_ITEM_ID)
 
-            val latestBid = bidService.getLatestBid(auctionItemId)!!
+            val latestBid = bidService.getLatestBid(AUCTION_ITEM_ID)!!
             val latestBidIdTag = page.locator("#last-bid-id")
             latestBidIdTag.getAttribute("value").shouldBe(latestBid.lastBidId)
         }
@@ -71,45 +96,73 @@ class PlaywrightTest(@Autowired val bidService: BidService) {
     @Test
     fun `Make a bid`() {
         playwright!!.chromium().launch().use { browser ->
-            val page = browser.newPage()
-            page.navigate("$baseUrl/")
-
-            // Locate and click the button to open the modal
-            val button = page.locator("[data-bs-itemid='b030b21b-73f9-40ff-8518-4a45f2c9b769']")
-            button.click()
-
-            // Wait for the modal to be visible
-            page.waitForSelector("#myBidModal.show")
-
-            // Set the bid amount
-            val bidAmountInput = page.locator("#bid-amount")
-            bidAmountInput.fill("42")
-
-            // Click the "Bid" button
-            val submitBidButton = page.locator("#submit-bid")
-            submitBidButton.click()
+            val page = browser.openAuctionItemModal()
+            page.placeBid("42")
 
             // Wait for the modal to be hidden
-            page.waitForSelector("#myBidModal", Page.WaitForSelectorOptions().setState(WaitForSelectorState.HIDDEN))
+            page.waitForSelector("#myBidModal", WaitForSelectorOptions().setState(WaitForSelectorState.HIDDEN))
 
             // Check that bid is found in database
-            val bid = bidService.getLatestBid("b030b21b-73f9-40ff-8518-4a45f2c9b769")!!
+            val bid = bidService.getLatestBid(AUCTION_ITEM_ID)!!
             bid.lastBidAmount shouldBe "42.00".toBigDecimal()
         }
+    }
 
-        @Test
-        fun `Attempt to make bid without bid amount shows error to user`(){
+    @Test
+    fun `User makes simultaneous bid and gets error`() {
+        playwright!!.chromium().launch().use { browser ->
+            val page = browser.openAuctionItemModal()
 
+            // Make a simultaneous bid
+            val latestBid = bidService.getLatestBid(AUCTION_ITEM_ID)
+            bidService.addBid(AUCTION_ITEM_ID, "test.bidder.bob@nitor.com", BigDecimal.TEN, latestBid!!.lastBidId).apply {
+                shouldBeRight()
+            }
+
+            // Make bid with outdated lastBidId
+            page.placeBid("42")
+
+            // Check if the alert box contains the correct error message
+            page.waitForSelector("#error-alert:visible")
+            val alertText = page.locator("#error-alert").textContent()
+            alertText shouldBe "Other user has placed a bid"
         }
+    }
 
-        @Test
-        fun `User makes simultaneous bid and gets error`(){
+    @Test
+    fun `Attempt to make bid without bid amount shows error to user`() {
+        playwright!!.chromium().launch().use { browser ->
+            val page = browser.openAuctionItemModal()
 
+            // Make bid with empty amount
+            page.placeBid("")
+
+            //Check if the invalid-feedback contains the correct error message
+            page.waitForSelector(".invalid-feedback:visible")
+            val errorMessage = page.locator(".invalid-feedback").textContent()
+            errorMessage shouldContain "Please enter a valid bid amount"
         }
+    }
 
-        @Test
-        fun `After successful bid auction item is updated with current price and latest bid`(){
+    @Test
+    fun `After successful bid auction item is updated with current price and latest bid`() {
+        playwright!!.chromium().launch().use { browser ->
+            val page = browser.openAuctionItemModal()
 
+            val latestBid = bidService.getLatestBid(AUCTION_ITEM_ID)!!
+            val expectedPrice = latestBid.currentPrice.add("11".toBigDecimal())
+
+            page.placeBid("11")
+
+            // Wait for the modal to be hidden
+            page.waitForSelector("#myBidModal", WaitForSelectorOptions().setState(WaitForSelectorState.HIDDEN))
+
+            // Wait for the page to reload and finish loading
+            page.waitForLoadState(LoadState.LOAD)
+
+            // Check that Current Price is updated to expectedPrice
+            val currentPriceText = page.locator("#item_${AUCTION_ITEM_ID} .card-text").textContent()
+            currentPriceText shouldContain "$expectedPrice€"
         }
     }
 
