9f4122b9451cebb1eeb635aa7ca668baf322e1d4
Zornak the Ruthless <petri.toivari@nitor.com>
2025-03-15 11:47:38 +0200

    Commit

--- src/main/kotlin/com/nitor/nauction/AuctionController.kt
+++ src/main/kotlin/com/nitor/nauction/AuctionController.kt
@@ -1,14 +1,18 @@
 package com.nitor.nauction
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+
+private val log = KotlinLogging.logger {}
 
 @Controller
 class AuctionController(val dao: AuctionDao) {
 
     @GetMapping("/")
     fun index(model: Model): String {
+        log.info { "Rendering index page" }
         model.addAttribute("items", dao.findAllOpen())
         return "index"
     }
